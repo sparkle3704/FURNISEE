f0e06e589379fcecb2db2d84afa811cb5b91fe19
Hoàng Đức Mạnh <hoangducmanh03072004@gmail.com>
2023-12-03 04:16:06 +0700

    added files

---
+++ sub/js/products.js
@@ -0,0 +1,19 @@
+const products = new Map()
+products.set('item-cart-1', [
+  {
+    full_img_url: 'images/chair/chair-1.jpg',
+    thumb_img_url: 'images/chair/chair-1-thumbnail.jpg',
+  },
+  {
+    full_img_url: 'images/chair/chair-2.jpg',
+    thumb_img_url: 'images/chair/chair-2-thumbnail.jpg',
+  },
+  {
+    full_img_url: 'images/chair/chair-3.jpg',
+    thumb_img_url: 'images/chair/chair-3-thumbnail.jpg',
+  },
+  {
+    full_img_url: 'images/chair/chair-4.jpg',
+    thumb_img_url: 'images/chair/chair-4-thumbnail.jpg',
+  },
+])
